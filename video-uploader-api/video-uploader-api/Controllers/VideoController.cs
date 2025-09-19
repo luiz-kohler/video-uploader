da77f424bfec9273215219e82cc8003211621f71
luiz-kohler <luizfelipekohler03@gmail.com>
2025-09-19 05:45:46 -0300

    feat: basic endpoints structure

---
+++ video-uploader-api/video-uploader-api/Controllers/VideoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace video_uploader_api.Controllers
+{
+    [Route("videos")]
+    public class VideoController : Controller
+    {
+        [HttpPost("upload")]
+        public IActionResult Upload()
+        {
+            return Ok();
+        }
+
+        [HttpPost("check-status")]
+        public IActionResult CheckStatus()
+        {
+            return Ok();
+        }
+    }
+}
