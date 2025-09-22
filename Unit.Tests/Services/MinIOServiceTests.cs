e0b32c3180612e525ee199f2767be898c6074ab5
luiz-kohler <luizfelipekohler03@gmail.com>
2025-09-22 07:26:16 -0300

    returning null when any error occur in minIO service

---
+++ Unit.Tests/Services/MinIOServiceTests.cs
@@ -0,0 +1,96 @@
+﻿using API.Services;
+using Bogus;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Minio;
+using Minio.DataModel.Args;
+using Minio.DataModel.Response;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using System.Net;
+
+namespace Unit.Tests.Services
+{
+    public class MinIOServiceTests
+    {
+        private readonly Faker _faker;
+        private readonly MinIOService _service;
+        private readonly MinIOVariables _minIOVaraibles;
+        private readonly IMinioClient _client;
+
+        public MinIOServiceTests()
+        {
+            _faker = new Faker();
+
+            var factory = Substitute.For<IMinioClientFactory>();
+            var options = Substitute.For<IOptions<MinIOVariables>>();
+
+            _client = Substitute.For<IMinioClient>();
+
+            _minIOVaraibles = new Faker<MinIOVariables>().Generate();
+            options.Value.Returns(_minIOVaraibles);
+
+            factory.CreateClient().Returns(_client);
+
+            _service = new MinIOService(factory, options);
+        }
+
+        [Fact]
+        public async Task Should_Upload_File_Successfully()
+        {
+            var expectedResponse = _faker.Random.String();
+            var bucketName = _minIOVaraibles.BucketName;
+
+            var putObjectResponse = new Faker<PutObjectResponse>()
+                .CustomInstantiator(f =>
+                    new PutObjectResponse(f.PickRandom<HttpStatusCode>(),
+                        f.Random.String(),
+                        new Dictionary<string, string>(),
+                        f.Random.Long(),
+                        expectedResponse)
+                );
+
+
+            var file = Substitute.For<IFormFile>();
+
+            _client.BucketExistsAsync(Arg.Any<BucketExistsArgs>()).Returns(false);
+            _client.PutObjectAsync(Arg.Any<PutObjectArgs>()).Returns(putObjectResponse);
+
+            var response = await _service.Upload(file);
+
+            await _client.Received(1).BucketExistsAsync(Arg.Any<BucketExistsArgs>());
+            await _client.Received(1).PutObjectAsync(Arg.Any<PutObjectArgs>());
+
+            response.Should().Be(expectedResponse);
+        }
+
+        [Fact]
+        public async Task Should_Return_Empty_String_When_Upload_File_Unsuccessfully()
+        {
+            var expectedResponse = string.Empty;
+            var bucketName = _minIOVaraibles.BucketName;
+
+            var putObjectResponse = new Faker<PutObjectResponse>()
+                .CustomInstantiator(f =>
+                    new PutObjectResponse(f.PickRandom<HttpStatusCode>(),
+                        f.Random.String(),
+                        new Dictionary<string, string>(),
+                        f.Random.Long(),
+                        expectedResponse)
+                );
+
+            var file = Substitute.For<IFormFile>();
+
+            _client.BucketExistsAsync(Arg.Any<BucketExistsArgs>()).Returns(false);
+            _client.PutObjectAsync(Arg.Any<PutObjectArgs>()).Throws<Exception>();
+
+            var response = await _service.Upload(file);
+
+            await _client.Received(1).BucketExistsAsync(Arg.Any<BucketExistsArgs>());
+            await _client.Received(1).PutObjectAsync(Arg.Any<PutObjectArgs>());
+
+            response.Should().Be(expectedResponse);
+        }
+    }
+}
