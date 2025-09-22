19daf440473f0705150dbfded8971ce96814918f
luiz-kohler <luizfelipekohler03@gmail.com>
2025-09-22 17:54:23 -0300

    implementing unit tests

---
+++ Tests.Integration/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,16 @@
+﻿using DotNet.Testcontainers.Builders;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Tests.Integration
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
+    {
+        private readonly ContainerBuilder _minIOContainer = new ContainerBuilder();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+        }
+    }
+}
