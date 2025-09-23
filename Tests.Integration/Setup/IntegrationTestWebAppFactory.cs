0e1fb071b49ed5fde83c505dc2311c92f610cb6b
luiz-kohler <luizfelipekohler03@gmail.com>
2025-09-23 18:57:26 -0300

    refac basic integration tests structure

---
+++ Tests.Integration/Setup/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,35 @@
+﻿using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.Minio;
+
+namespace Tests.Integration.Setup
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public HttpClient HttpClient { get; private set; } = null!;
+
+        private readonly IContainer _minioContainer = new MinioBuilder()
+                                                         .WithImage("quay.io/minio/minio")
+                                                         .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            Environment.SetEnvironmentVariable("MinIO:Endpoint", $"localhost:{_minioContainer.GetMappedPublicPort()}");
+            Environment.SetEnvironmentVariable("MinIO:AccessKey", $"minioadmin");
+            Environment.SetEnvironmentVariable("MinIO:SecretKey", $"minioadmin ");
+
+            HttpClient = CreateClient();
+        }
+        public async Task InitializeAsync()
+        {
+            await _minioContainer.StartAsync();   
+        }
+
+        public new async Task DisposeAsync()
+        {
+            await _minioContainer.DisposeAsync();
+        }
+    }
+}
