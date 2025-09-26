638e677b55d719cc625750a57a990962ee3440d1
luiz-kohler <luizfelipekohler03@gmail.com>
2025-09-26 11:37:26 -0300

    implementing validation of file type w/ tests

---
+++ Tests/Tests.Unit/Setup/Builders/IFormFileBuilder.cs
@@ -0,0 +1,85 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using System.Text;
+
+namespace Tests.Unit.Setup.Builders
+{
+    public class IFormFileBuilder
+    {
+        private readonly Faker _faker;
+
+        private string _contentType;
+        private string _fileName;
+        private int? _length;
+
+        public IFormFileBuilder()
+        {
+            _faker = new Faker();
+        }
+
+        public IFormFileBuilder WithFileName(string fileName)
+        {
+            _fileName = fileName;
+            return this;
+        }
+
+        public IFormFileBuilder WithLength(int length)
+        {
+            _length = length;
+            return this;
+        }
+
+        public IFormFileBuilder WithContentType(string contentType)
+        {
+            _contentType = contentType;
+            return this;
+        }
+
+        public IFormFile Build()
+        {
+            var file = CreateMockMp4File();
+
+            file.FileName.Returns(_fileName ?? _faker.Random.String());
+            file.Length.Returns(_length ?? _faker.Random.Number(100, 1000));
+            file.ContentType.Returns(_contentType ?? "video/mp4");
+
+            return file;
+        }
+
+
+        private static IFormFile CreateMockMp4File()
+        {
+            var videoBytes = CreateMinimalMp4Bytes(1024);
+            var stream = new MemoryStream(videoBytes);
+
+            var file = Substitute.For<IFormFile>();
+            file.OpenReadStream().Returns(stream);
+
+            file.CopyToAsync(Arg.Any<Stream>(), Arg.Any<CancellationToken>())
+                .Returns(x =>
+                {
+                    var targetStream = x.Arg<Stream>();
+                    var token = x.Arg<CancellationToken>();
+                    return stream.CopyToAsync(targetStream, token);
+                });
+
+            return file;
+        }
+
+        private static byte[] CreateMinimalMp4Bytes(int fileSizeKB)
+        {
+            var bytes = new List<byte>();
+
+            bytes.AddRange(Encoding.ASCII.GetBytes("ftypmp42"));
+
+            while (bytes.Count < fileSizeKB * 1024)
+            {
+                bytes.AddRange(Encoding.ASCII.GetBytes("mdat"));
+            }
+
+            return bytes.Take(fileSizeKB * 1024).ToArray();
+        }
+    }
+
+}
---
+++ Tests/Tests.Unit/Tests/Controllers/VideoControllerTests.cs
@@ -0,0 +1,116 @@
+﻿using API.Controllers;
+using API.Services;
+using Bogus;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Tests.Unit.Setup.Builders;
+
+namespace Tests.Unit.Tests.Controllers
+{
+    [Trait("Category", "Unit")]
+    [Collection("VideoController")]
+    public class VideoControllerTests
+    {
+        private readonly Faker _faker;
+        private readonly VideoController _controller;
+        private readonly IObjectStorageService _service;
+
+        public VideoControllerTests()
+        {
+            _faker = new Faker();
+            _service = Substitute.For<IObjectStorageService>();
+            _controller = new VideoController(_service);
+        }
+
+        [Fact]
+        public async Task Upload_WithValidFile_ShouldReturnFileIdSuccessfully()
+        {
+            var fileId = _faker.Random.String();
+
+            var file = new IFormFileBuilder()
+                .Build();
+
+            _service.Upload(Arg.Any<IFormFile>()).Returns(fileId);
+
+            var response = await _controller.Upload(file);
+
+            await _service.Received(1).Upload(Arg.Any<IFormFile>());
+
+            response.Should().NotBeNull();
+            var acceptedResult = response as AcceptedResult;
+            acceptedResult.Should().NotBeNull();
+            acceptedResult.StatusCode.Should().Be(202);
+            response.Should().BeOfType<AcceptedResult>()
+            .Which.Value.Should().BeEquivalentTo(new { fileId });
+        }
+
+        [Fact]
+        public async Task Upload_WithNullFile_ShouldReturnBadRequest()
+        {
+            var expectedMessage = "File must be informed";
+
+            var response = await _controller.Upload(null);
+
+            var badRequestResult = response.Should().BeOfType<BadRequestObjectResult>().Subject;
+            badRequestResult.StatusCode.Should().Be(400);
+            badRequestResult.Value.Should().Be(expectedMessage);
+        }
+
+        [Fact]
+        public async Task Upload_WithEmptyFile_ShouldReturnBadRequest()
+        {
+            var expectedMessage = "File must be informed";
+
+            var file = new IFormFileBuilder()
+                .WithLength(0)
+                .Build();
+
+            var response = await _controller.Upload(file);
+
+            var badRequestResult = response.Should().BeOfType<BadRequestObjectResult>().Subject;
+            badRequestResult.StatusCode.Should().Be(400);
+            badRequestResult.Value.Should().Be(expectedMessage);
+        }
+
+        [Fact]
+        public async Task Upload_WithNonMp4File_ShouldReturnBadRequest()
+        {
+            var expectedMessage = "File must be .mp4";
+
+            var file = new IFormFileBuilder()
+                .WithContentType("image/png")
+                .Build();
+
+            var response = await _controller.Upload(file);
+
+            var badRequestResult = response.Should().BeOfType<BadRequestObjectResult>().Subject;
+            badRequestResult.StatusCode.Should().Be(400);
+            badRequestResult.Value.Should().Be(expectedMessage);
+        }
+
+        [Fact]
+        public async Task Upload_WithServiceThrowingException_ShouldReturnInternalServerError()
+        {
+            var expectedExceptionMessage = _faker.Random.String();
+            var exception = new Exception(expectedExceptionMessage);
+
+            var file = new IFormFileBuilder()
+                .Build();
+
+            _service.Upload(Arg.Any<IFormFile>()).Throws(exception);
+
+            var response = await _controller.Upload(file);
+
+            await _service.Received(1).Upload(Arg.Any<IFormFile>());
+
+            var objectResult = response.Should().BeOfType<ObjectResult>().Subject;
+            objectResult.StatusCode.Should().Be(500);
+            var problemDetails = objectResult.Value as ProblemDetails;
+            problemDetails.Should().NotBeNull();
+            problemDetails.Detail.Should().Be(expectedExceptionMessage);
+        }
+    }
+}
